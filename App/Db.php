3da01650a778032de640b255b4d4536ab1cafb19
sitesoft1 <sitesoft1@gmail.com>
2019-09-08 18:51:56 +0300

    bd

--- App/Db.php
+++ App/Db.php
@@ -6,7 +6,9 @@ namespace App;
 
 class Db
 {
+    public $dbh;
    public function __construct(){
        echo 'Hello Db!!!';
+       $this->dbh = new \PDO('mysql:host=localhost;dbname=test;', 'root', '');
    }
 }
